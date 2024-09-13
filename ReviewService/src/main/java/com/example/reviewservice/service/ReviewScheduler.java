4d477dd3bbdac57474053a32a409edb57ed379c5
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-09-13 04:15:30 +0300

    add ReviewScheduler Scheduled 5

--- ReviewService/src/main/java/com/example/reviewservice/service/ReviewScheduler.java
+++ ReviewService/src/main/java/com/example/reviewservice/service/ReviewScheduler.java
@@ -13,9 +13,14 @@ public class ReviewScheduler {
     private final ReviewService reviewService;
 
 
-    @Scheduled(cron = "0 */4 * * * *") // Выполняется каждые 4 минуты
+    //    @Scheduled(cron = "0 */4 * * * *") // Выполняется каждые 4 минуты
+//    public void checkReviews() {
+//        List<ReviewDto> reviews = reviewService.getAllReviews();
+//        reviewService.checkAndDeleteInappropriateReviews(reviews);
+//    }
+    @Scheduled(cron = "0 0 */5 * * *") // Выполняется каждые 5 часов
     public void checkReviews() {
-        List<ReviewDto> reviews = reviewService.getAllReviews();
+        List<ReviewDto> reviews = reviewService.getAllReviews(); // Метод для получения всех отзывов
         reviewService.checkAndDeleteInappropriateReviews(reviews);
     }
 }
